2da09ef2ccd89cac8d3ce34a9fc92c432f8bdd0b
Mitocapi <marcocapi01@gmail.com>
2022-05-06 11:00:40 +0200

    ho aggiunto un main

---
+++ GameG/src/main/java/provaClasse.java
@@ -0,0 +1,15 @@
+public class provaClasse {
+    public static int somma(){
+        int x= 1;
+        int y = 2;
+        return x+y;
+
+    }
+
+    public static void main(String[] args) {
+        int a = somma();
+
+        System.out.println(a);
+    }
+
+}
