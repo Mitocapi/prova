d643d91008ca5b0e8b2c6671f89c7a6041594cab
samuele <Samuele.costantini01@gmail.com>
2022-05-20 19:29:34 +0200

    esempio di progetto per biliardo

---
+++ GameG/src/main/java/bili/Extras.java
@@ -0,0 +1,4 @@
+package bili;
+
+public class Extras {
+}
---
+++ GameG/src/main/java/bili/StatusPanel.java
@@ -0,0 +1,22 @@
+package bili;
+
+import javax.swing.*;
+//import java.awt.*;
+import java.awt.event.*;
+
+public class StatusPanel extends JPanel {
+
+    JButton btnStart;
+//	TablePanel tp;
+
+    public StatusPanel(final TablePanel tp) {
+
+//		this.tp = tp;
+
+
+
+
+    }
+
+
+}
